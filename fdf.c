f1e1033e204a0665d40b6a44e8908918378963bb
Marsha <151203981+marsha-t@users.noreply.github.com>
2024-02-28 07:06:14 +0000

    update mlx with version from school

--- fdf.c
+++ fdf.c
@@ -6,11 +6,11 @@
 /*   By: mateo <mateo@student.42abudhabi.ae>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/26 11:14:06 by mateo             #+#    #+#             */
-/*   Updated: 2024/02/26 18:24:03 by mateo            ###   ########.fr       */
+/*   Updated: 2024/02/28 06:27:35 by mateo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "minilibx-linux/mlx.h"
+#include "mlx/mlx.h"
 #include <stdio.h>
 
 int	main(void)
@@ -19,9 +19,7 @@ int	main(void)
 	void	*mlx_window;
 
 	mlx_connection = mlx_init();
-	if (!mlx_connection)
-		printf("failed malloc?\n");
-	// mlx_window = mlx_new_window(mlx_connection, 50, 50, "My Window");
-	// mlx_loop(mlx_connection);
+	mlx_window = mlx_new_window(mlx_connection, 50, 50, "My Window");
+	mlx_loop(mlx_connection);
 	return (0);
 }
